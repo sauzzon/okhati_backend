317aba793b7009c31831fed126ffd6726f74b5bd
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-02-18 16:50:03 +0545

    Update app.js
    routes, middlewares, db connection implemented in app.js

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
-const port = 8000;
+const connectDatabase = require("./database/connect");
+const authenticateUser = require("./middleware/authentication");
+
+// routers
+const authRouter = require("./routes/authentication");
+const userRouter = require("./routes/users");
+
+// error handler
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+// routes
+app.use("/api/auth", authRouter);
+app.use("/api/users", authenticateUser, userRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 8000;
 
 const startServer = async () => {
   try {
+    await connectDatabase(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening to port ${port}!!!`)
     );
