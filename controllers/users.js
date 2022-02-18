1457a9a995ad4d898ef4fd7928bd5b03d5a56d5f
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-02-18 23:06:41 +0545

    find replaced by findOne

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 
 const getDashboard = async (req, res) => {
-  const user = await User.find({ _id: req.user.userId });
+  const user = await User.findOne({ _id: req.user.userId });
   res.status(StatusCodes.OK).json({ user });
 };
 
