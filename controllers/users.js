d3470aeb3505caf4dd96bbe64255c29d36ca3a0a
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-02-18 16:39:23 +0545

    routes and middlewares added
    1. authentication and users routes added
    2. middlewares for authentication, errors, not-found added

---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
+const getDashboard = async (req, res) => {
+  const user = await User.find({ _id: req.user.userId });
+  res.status(StatusCodes.OK).json({ user });
+};
+
+module.exports = {
+  getDashboard,
+};
